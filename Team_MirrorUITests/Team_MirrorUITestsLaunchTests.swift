91879666fca8878c57353b896ba7e4ad30cc6cb9
SaintBrady <brayden.dyke@gmail.com>
2023-04-28 14:36:46 -0500

    Initial Commit

---
+++ Team_MirrorUITests/Team_MirrorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Team_MirrorUITestsLaunchTests.swift
+//  Team_MirrorUITests
+//
+//  Created by Brayden M Dyke on 4/28/23.
+//
+
+import XCTest
+
+final class Team_MirrorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
